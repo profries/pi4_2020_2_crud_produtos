6c4471b9a6b2ff779ade7d25e2a672e62e325ef1
Luis <luisries.zeta@gmail.com>
2020-08-27 21:31:10 -0300

    Exercicio 3

--- app.js
+++ app.js
@@ -2,24 +2,58 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 app.get('/produtos', (req, res) => {
-  res.send('GET')
+  res.json([{id:1, nome:'prod1', preco: 30.00}]);
 })
 
 app.post('/produtos', (req, res) => {
-    res.send('POST')
+    console.log(req.body);
+    console.log(req.body.nome);
+    console.log(req.body.preco);
+    res.status(201).json(req.body);
 })
 
 app.put('/produtos/:id', (req, res) => {
-    res.send('PUT'+req.params.id)
+    if(req.params.id == 1){
+        let produto = req.body;
+        produto.preco += 5;
+        res.json(produto);
+    }
+    else {
+        res.sendStatus(404);
+    }
+    
 })
   
 app.delete('/produtos/:id', (req, res) => {
-    res.send('DELETE'+req.params.id)
+    if(req.params.id == 1){
+        let produto = new Object();
+        produto.id = 1;
+        produto.nome = 'prod1';
+        produto.preco = 30.00;
+        res.json(produto);
+    }
+    else {
+        res.sendStatus(404);
+    }
 })
     
 app.get('/produtos/:id', (req, res) => {
-    res.send('GET'+req.params.id)
+    if(req.params.id == 1){
+        let produto = new Object();
+        produto.id = 1;
+        produto.nome = 'prod1';
+        produto.preco = 30.00;
+        res.json(produto);
+    }
+    else {
+        res.sendStatus(404);
+    }
+    
 })
   
 app.listen(port, () => {
